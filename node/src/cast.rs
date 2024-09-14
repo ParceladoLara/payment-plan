ef0728f2fdb136cb71b2e0ae8c4f8dd364c9db61
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2024-09-13 23:18:55 -0300

    feat: down payment

--- node/src/cast.rs
+++ node/src/cast.rs
@@ -1,4 +1,4 @@
-use core_payment_plan::{Params, Response};
+use core_payment_plan::{DownPaymentParams, DownPaymentResponse, Params, Response};
 
 use chrono::NaiveTime;
 use neon::{
@@ -179,6 +179,95 @@ pub fn cast_vec_response_to_js_array<'a, C: Context<'a>>(
     let array = JsArray::new(cx, responses.len() as usize);
     for (i, response) in responses.into_iter().enumerate() {
         let obj = cast_response_to_js_object(cx, response)?;
+        array.set(cx, i as u32, obj)?;
+    }
+    Ok(array)
+}
+
+pub fn cast_js_object_to_down_payment_param(
+    cx: &mut FunctionContext,
+    obj: Handle<JsObject>,
+) -> NeonResult<DownPaymentParams> {
+    let params: Handle<JsObject> = obj.get(cx, "params")?;
+    println!("js params {:?}", params);
+    let request_amount: Handle<JsValue> = obj.get(cx, "requestedAmount")?;
+    println!("js requested amount{:?}", request_amount);
+    let min_installment_amount: Handle<JsValue> = obj.get(cx, "minInstallmentAmount")?;
+    println!("js min installment{:?}", min_installment_amount);
+    let first_payment_date_millis: Handle<JsDate> = obj.get(cx, "firstPaymentDate")?;
+    println!("js payment date{:?}", first_payment_date_millis);
+    let installments: Handle<JsValue> = obj.get(cx, "installments")?;
+    println!("js installments{:?}", installments);
+
+    let params = cast_js_object_to_param(cx, params)?;
+    println!("params {:?}", params);
+    let request_amount = any_to_number(cx, request_amount)?;
+    println!("requested amount {:?}", request_amount);
+    let min_installment_amount = any_to_number(cx, min_installment_amount)?;
+    println!("min installment amount {:?}", min_installment_amount);
+    let first_payment_date_millis = first_payment_date_millis.value(cx);
+    println!("first payment date {:?}", first_payment_date_millis);
+    let installments = any_to_number(cx, installments)? as u32;
+    println!("installments {:?}", installments);
+
+    let first_payment_date =
+        chrono::DateTime::from_timestamp_millis(first_payment_date_millis as i64);
+    let first_payment_date = match first_payment_date {
+        Some(date) => date.date_naive(),
+        None => {
+            return cx.throw_error("Invalid date");
+        }
+    };
+
+    Ok(DownPaymentParams {
+        params,
+        request_amount,
+        min_installment_amount,
+        first_payment_date,
+        installments,
+    })
+}
+
+fn cast_down_payment_response_to_js_object<'a, C: Context<'a>>(
+    cx: &mut C,
+    response: DownPaymentResponse,
+) -> NeonResult<Handle<'a, JsObject>> {
+    let installment_amount = JsNumber::new(cx, response.installment_amount);
+    let total_amount = JsNumber::new(cx, response.total_amount);
+    let installment_quantity = JsNumber::new(cx, response.installment_quantity as f64);
+
+    let first_payment_date = response
+        .first_payment_date
+        .and_time(NaiveTime::from_hms_opt(3, 0, 0).unwrap())
+        .and_utc();
+
+    let first_payment_date = JsDate::new(cx, first_payment_date.timestamp_millis() as f64);
+    let first_payment_date = match first_payment_date {
+        Ok(date) => date,
+        Err(_) => {
+            return cx.throw_error("Invalid date");
+        }
+    };
+
+    let plans = cast_vec_response_to_js_array(cx, response.plans)?;
+
+    let obj = JsObject::new(cx);
+    obj.set(cx, "installmentAmount", installment_amount)?;
+    obj.set(cx, "totalAmount", total_amount)?;
+    obj.set(cx, "installmentQuantity", installment_quantity)?;
+    obj.set(cx, "firstPaymentDate", first_payment_date)?;
+    obj.set(cx, "plans", plans)?;
+
+    Ok(obj)
+}
+
+pub fn cast_vec_down_payment_response_to_js_array<'a, C: Context<'a>>(
+    cx: &mut C,
+    responses: Vec<DownPaymentResponse>,
+) -> NeonResult<Handle<'a, JsArray>> {
+    let array = JsArray::new(cx, responses.len() as usize);
+    for (i, response) in responses.into_iter().enumerate() {
+        let obj = cast_down_payment_response_to_js_object(cx, response)?;
         array.set(cx, i as u32, obj)?;
     }
     Ok(array)
