6855c899878b0290af0fd7a71530bc166c4ec5a0
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2024-09-26 01:20:33 -0300

    test: bin tests

--- bin/test/bin_test.go
+++ bin/test/bin_test.go
@@ -12,19 +12,22 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+var firstPaymentDate = time.Date(2022, time.April, 18, 0, 0, 0, 0, time.Local)
+var requestedDate = time.Date(2022, time.March, 18, 0, 0, 0, 0, time.Local)
+
 var param = protos.PlanParams{
-	RequestedAmount:        7431,
-	FirstPaymentDateMillis: time.Date(2024, time.October, 24, 0, 0, 0, 0, time.UTC).UnixMilli(),
-	RequestedDateMillis:    time.Date(2024, time.February, 24, 0, 0, 0, 0, time.UTC).UnixMilli(),
-	Installments:           18,
+	RequestedAmount:        8800,
+	FirstPaymentDateMillis: firstPaymentDate.UnixMilli(),
+	RequestedDateMillis:    requestedDate.UnixMilli(),
+	Installments:           24,
 	DebitServicePercentage: 0,
 	Mdr:                    0.05,
 	TacPercentage:          0,
 	IofOverall:             0.0038,
 	IofPercentage:          0.03,
-	InterestRate:           0.04,
+	InterestRate:           0.0235,
 	MinInstallmentAmount:   100,
-	MaxTotalAmount:         100000,
+	MaxTotalAmount:         1000000,
 }
 
 var downPaymentParams = protos.DownPaymentParams{
@@ -35,13 +38,84 @@ var downPaymentParams = protos.DownPaymentParams{
 	MinInstallmentAmount:   100,
 }
 
+type expectedValues struct {
+	ContractAmount             float64
+	ContractAmountWithoutTac   float64
+	CustomerAmount             float64
+	CustomerDebitServiceAmount float64
+	DebitService               float64
+	EirMonthly                 float64
+	EirYearly                  float64
+	EffectiveInterestRate      float64
+	Installment                int
+	InstallmentAmount          float64
+	InterestRate               float64
+	MdrAmount                  float64
+	MerchantDebitServiceAmount float64
+	MerchantTotalAmount        float64
+	SettledToMerchant          float64
+	TecMonthly                 float64
+	TecYearly                  float64
+	TotalAmount                float64
+	TotalIof                   float64
+}
+
 func TestBMPPlan(t *testing.T) {
+
+	expected := expectedValues{
+		ContractAmount:             9037.318869753424,
+		ContractAmountWithoutTac:   9037.318869753424,
+		CustomerAmount:             499.1987614851067,
+		CustomerDebitServiceAmount: 2943.451405889136,
+		DebitService:               2943.451405889136,
+		EirMonthly:                 0.024085088183680048,
+		EirYearly:                  0.33055401101326365,
+		EffectiveInterestRate:      0.024085088183680048,
+		Installment:                24,
+		InstallmentAmount:          499.1987614851067,
+		InterestRate:               0.0235,
+		MdrAmount:                  440.0,
+		MerchantDebitServiceAmount: 0.0,
+		MerchantTotalAmount:        440.0,
+		SettledToMerchant:          8360.0,
+		TecMonthly:                 0.025868426671143974,
+		TecYearly:                  0.3586261331729559,
+		TotalAmount:                11980.77027564256,
+		TotalIof:                   237.3188697534247,
+	}
+
 	var plan protos.PlanResponses
 
 	err := callBuff(&param, &plan)
 	if err != nil {
 		t.Errorf("Error running payment-plan CLI: %s", err)
 	}
+
+	t.Run("Success", func(t *testing.T) {
+
+		planHelper(t, &plan, &expected)
+	})
+
+	t.Run("invalid installments", func(t *testing.T) {
+		oldInstallments := param.Installments
+		param.Installments = 0
+		err := callBuff(&param, &plan)
+		param.Installments = oldInstallments
+		if err == nil {
+			t.Errorf("Expected error, got none")
+		}
+	})
+
+	t.Run("invalid requested amount", func(t *testing.T) {
+		oldRequestedAmount := param.RequestedAmount
+		param.RequestedAmount = 0
+		err := callBuff(&param, &plan)
+		param.RequestedAmount = oldRequestedAmount
+		if err == nil {
+			t.Errorf("Expected error, got none")
+		}
+	})
+
 }
 func TestQiTechPlan(t *testing.T) {
 	var plan protos.PlanResponses
@@ -75,6 +149,88 @@ func TestQiTechDownPayment(t *testing.T) {
 	downPaymentHelper(t, &plan)
 }
 
+func planHelper(t *testing.T, plan *protos.PlanResponses, expected *expectedValues) {
+
+	if len(plan.Responses) != expected.Installment {
+		t.Fatalf("Expected %d response, got %d", expected.Installment, len(plan.Responses))
+	}
+
+	response := plan.Responses[expected.Installment-1]
+
+	if response.ContractAmount != expected.ContractAmount {
+		t.Errorf("Expected contract amount to be %f, got %f", expected.ContractAmount, response.ContractAmount)
+	}
+
+	if response.ContractAmountWithoutTac != expected.ContractAmountWithoutTac {
+		t.Errorf("Expected contract amount without tac to be %f, got %f", expected.ContractAmountWithoutTac, response.ContractAmountWithoutTac)
+	}
+
+	if response.CustomerAmount != expected.CustomerAmount {
+		t.Errorf("Expected customer amount to be %f, got %f", expected.CustomerAmount, response.CustomerAmount)
+	}
+
+	if response.CustomerDebitServiceAmount != expected.CustomerDebitServiceAmount {
+		t.Errorf("Expected customer debit service amount to be %f, got %f", expected.CustomerDebitServiceAmount, response.CustomerDebitServiceAmount)
+	}
+
+	if response.DebitService != expected.DebitService {
+		t.Errorf("Expected debit service to be %f, got %f", expected.DebitService, response.DebitService)
+	}
+
+	if response.EirMonthly != expected.EirMonthly {
+		t.Errorf("Expected EIR monthly to be %f, got %f", expected.EirMonthly, response.EirMonthly)
+	}
+
+	if response.EirYearly != expected.EirYearly {
+		t.Errorf("Expected EIR yearly to be %f, got %f", expected.EirYearly, response.EirYearly)
+	}
+
+	if response.EffectiveInterestRate != expected.EffectiveInterestRate {
+		t.Errorf("Expected effective interest rate to be %f, got %f", expected.EffectiveInterestRate, response.EffectiveInterestRate)
+	}
+
+	if response.InstallmentAmount != expected.InstallmentAmount {
+		t.Errorf("Expected installment amount to be %f, got %f", expected.InstallmentAmount, response.InstallmentAmount)
+	}
+
+	if response.InterestRate != expected.InterestRate {
+		t.Errorf("Expected interest rate to be %f, got %f", expected.InterestRate, response.InterestRate)
+	}
+
+	if response.MdrAmount != expected.MdrAmount {
+		t.Errorf("Expected MDR amount to be %f, got %f", expected.MdrAmount, response.MdrAmount)
+	}
+
+	if response.MerchantDebitServiceAmount != expected.MerchantDebitServiceAmount {
+		t.Errorf("Expected merchant debit service amount to be %f, got %f", expected.MerchantDebitServiceAmount, response.MerchantDebitServiceAmount)
+	}
+
+	if response.MerchantTotalAmount != expected.MerchantTotalAmount {
+		t.Errorf("Expected merchant total amount to be %f, got %f", expected.MerchantTotalAmount, response.MerchantTotalAmount)
+	}
+
+	if response.SettledToMerchant != expected.SettledToMerchant {
+		t.Errorf("Expected settled to merchant to be %f, got %f", expected.SettledToMerchant, response.SettledToMerchant)
+	}
+
+	if response.TecMonthly != expected.TecMonthly {
+		t.Errorf("Expected TEC monthly to be %f, got %f", expected.TecMonthly, response.TecMonthly)
+	}
+
+	if response.TecYearly != expected.TecYearly {
+		t.Errorf("Expected TEC yearly to be %f, got %f", expected.TecYearly, response.TecYearly)
+	}
+
+	if response.TotalAmount != expected.TotalAmount {
+		t.Errorf("Expected total amount to be %f, got %f", expected.TotalAmount, response.TotalAmount)
+	}
+
+	if response.TotalIof != expected.TotalIof {
+		t.Errorf("Expected total IOF to be %f, got %f", expected.TotalIof, response.TotalIof)
+	}
+
+}
+
 func downPaymentHelper(t *testing.T, plan *protos.DownPaymentResponses) {
 	if len(plan.Responses) != 2 {
 		t.Fatalf("Expected 2 responses, got %d", len(plan.Responses))
@@ -82,12 +238,12 @@ func downPaymentHelper(t *testing.T, plan *protos.DownPaymentResponses) {
 
 	response := plan.Responses[0]
 	if response.InstallmentAmount != 200 {
-		t.Fatalf("Expected installment amount to be 200, got %f", response.InstallmentAmount)
+		t.Errorf("Expected installment amount to be 200, got %f", response.InstallmentAmount)
 	}
 
 	plans := response.Plans.Responses
 	if len(plans) == 0 {
-		t.Fatalf("Expected at least one plan, got none")
+		t.Errorf("Expected at least one plan, got none")
 	}
 
 	firstPlan := plans[0]
@@ -95,17 +251,17 @@ func downPaymentHelper(t *testing.T, plan *protos.DownPaymentResponses) {
 	planDueDate := time.Date(2022, time.July, 20, 3, 0, 0, 0, time.UTC)
 	actualDueDate := time.UnixMilli(firstPlan.DueDateMillis).UTC()
 	if actualDueDate != planDueDate {
-		t.Fatalf("Expected first plan due date to be %s, got %s", planDueDate, actualDueDate)
+		t.Errorf("Expected first plan due date to be %s, got %s", planDueDate, actualDueDate)
 	}
 
 	response2 := plan.Responses[1]
 	if response2.InstallmentAmount != 100 {
-		t.Fatalf("Expected installment amount to be 100, got %f", response2.InstallmentAmount)
+		t.Errorf("Expected installment amount to be 100, got %f", response2.InstallmentAmount)
 	}
 
 	plans2 := response2.Plans.Responses
 	if len(plans2) == 0 {
-		t.Fatalf("Expected at least one plan, got none")
+		t.Errorf("Expected at least one plan, got none")
 	}
 
 	firstPlan2 := plans2[0]
@@ -113,7 +269,7 @@ func downPaymentHelper(t *testing.T, plan *protos.DownPaymentResponses) {
 	planDueDate2 := time.Date(2022, time.August, 20, 3, 0, 0, 0, time.UTC)
 	actualDueDate2 := time.UnixMilli(firstPlan2.DueDateMillis).UTC()
 	if actualDueDate2 != planDueDate2 {
-		t.Fatalf("Expected first plan due date to be %s, got %s", planDueDate2, actualDueDate2)
+		t.Errorf("Expected first plan due date to be %s, got %s", planDueDate2, actualDueDate2)
 	}
 }
 
