b690d625183d8a900976026ad86ffe4ca196daa5
Victor Ribeiro de Lima <victor@DESKTOP-4VLO0U0.>
2025-02-25 21:38:12 -0300

    chore: ajudando caio

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,43 +1,42 @@
-use core_payment_plan::{calculate_payment_plan, Params};
+use core_payment_plan::{calculate_payment_plan, next_disbursement_date, Params};
 
 fn main() {
-    let i = vec![64];
+    let today = chrono::Local::now().date_naive();
+    let next_disbursement_date = next_disbursement_date(today).unwrap();
+    let first_payment_date = chrono::NaiveDate::from_ymd_opt(2025, 03, 22).unwrap();
 
-    for i in i {
-        let requested_date = chrono::NaiveDate::from_ymd_opt(2024, 10, 23).unwrap();
+    let requested_amount = 4999.99;
+    let installments = 12;
+    let interest_rate = 0.0399; //Interest rate do caiao da massa
 
-        let first_payment_date = chrono::NaiveDate::from_ymd_opt(2024, 11, 23).unwrap();
+    let params = Params {
+        max_total_amount: f64::MAX,
+        min_installment_amount: 100.0,
+        requested_amount,
+        first_payment_date,
+        requested_date: next_disbursement_date,
+        installments,
+        debit_service_percentage: 0,
+        mdr: 0.1,
+        tac_percentage: 0.02,
+        iof_overall: 0.0038,      // %0.38
+        iof_percentage: 0.000082, // 0.0082%
+        interest_rate,
+        disbursement_only_on_business_days: true,
+    };
 
-        let requested_amount = 12853.43;
-        let installments = i;
-        let interest_rate = 0.035;
+    let mut result = calculate_payment_plan(params).unwrap();
 
-        let params = Params {
-            max_total_amount: f64::MAX,
-            min_installment_amount: 100.0,
-            requested_amount,
-            first_payment_date,
-            requested_date,
-            installments,
-            debit_service_percentage: 0,
-            mdr: 0.05,
-            tac_percentage: 0.0,
-            iof_overall: 0.0038,      // %0.38
-            iof_percentage: 0.000082, // 0.0082%
-            interest_rate,
-            disbursement_only_on_business_days: false,
-        };
+    println!("Length: {}", result.len());
 
-        let mut result = calculate_payment_plan(params).unwrap();
+    let result = result.pop().unwrap();
 
-        println!("Length: {}", result.len());
+    println!("Installment: {:#?}", result);
 
-        let result = result.pop().unwrap();
-
-        println!("CET: {}", result.total_effective_cost);
-        println!("annual_cet: {}", result.tec_yearly);
-        println!("installment_amount: {}", result.installment_amount);
-        println!("IOF: {}", result.total_iof);
-        println!("Contract Amount: {}", result.contract_amount);
-    }
+    println!("CET: {}", result.total_effective_cost);
+    println!("annual_cet: {}", result.tec_yearly);
+    println!("monthly_cet: {}", result.tec_monthly);
+    println!("installment_amount: {}", result.installment_amount);
+    println!("IOF: {}", result.total_iof);
+    println!("Contract Amount: {}", result.total_amount);
 }
